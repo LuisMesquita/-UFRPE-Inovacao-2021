6c43cdccbf6b2c2c6172b69dbd53a24a986c29ab
Helton Vasconcelos <37989368+helton886@users.noreply.github.com>
2021-06-28 22:03:44 -0300

    Revert "some changes and add dropdown calamilty filter"
    This reverts commit 9f51e2e6f49bb2f7d955508a21c53a5ae81205a6.

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Map from '../Map'
 import CalamityModal from '../../components/Modals/CalamityModal'
-import Filters from '../../components/Filters'
 
 function Home() {
   return (
     <div className='home'>
       <Map />
       <CalamityModal />
-      <Filters />
     </div>
   )
 }
