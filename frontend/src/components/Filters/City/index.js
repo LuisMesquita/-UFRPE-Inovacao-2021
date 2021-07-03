a9c98afb78fbe068d69ebb012da812c8fd93076b
Elivelton Rodrigues <elivelton.rodrigues@liferay.com>
2021-07-03 19:12:20 -0300

    added date filters and some changes

--- frontend/src/components/Filters/City/index.js
+++ frontend/src/components/Filters/City/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ClaySelect } from '@clayui/form'
+import ClayForm, { ClaySelect } from '@clayui/form'
 import { useSelector } from 'react-redux'
 
 import './index.scss'
 
-function City({ valueProp, onChange }) {
+function City({ valueProp, onChange, label }) {
   const { cities } = useSelector((state) => state.calamities.filter)
   const options = [
     { label: 'Cidade', value: null },
@@ -14,16 +14,23 @@ function City({ valueProp, onChange }) {
 
   return (
     <div className='city-filter'>
-      <ClaySelect value={valueProp} onChange={(value) => onChange(value)}>
-        {options.map((item) => (
-          <ClaySelect.Option
-            placeholder='Selecione'
-            key={item.value}
-            label={item.label}
-            value={item.value}
-          />
-        ))}
-      </ClaySelect>
+      <ClayForm.Group>
+        <label htmlFor='city'>{label}</label>
+        <ClaySelect
+          id='city'
+          value={valueProp}
+          onChange={(value) => onChange(value)}
+        >
+          {options.map((item) => (
+            <ClaySelect.Option
+              placeholder='Selecione'
+              key={item.value}
+              label={item.label}
+              value={item.value}
+            />
+          ))}
+        </ClaySelect>
+      </ClayForm.Group>
     </div>
   )
 }
@@ -31,11 +38,13 @@ function City({ valueProp, onChange }) {
 City.defaultProps = {
   onChange: () => {},
   valueProp: '',
+  label: 'Cidade',
 }
 
 City.propTypes = {
   onChange: PropTypes.func,
   valueProp: PropTypes.string,
+  label: PropTypes.string,
 }
 
 export default City
